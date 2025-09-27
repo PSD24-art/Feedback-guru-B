e7b93b7109dc140bd6347a985773543a48a40fc8
Prathamesh <pratham.dahakepsd@gmail.com>
2025-09-27 21:24:30 +0530

    bug fixes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 //
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://feedback-guru.onrender.com"],
+    origin: "https://feedback-guru.onrender.com",
     credentials: true,
   })
 );
